df7884584291d0c29ed449a17fb6980e895f4f43
nguyenvanhoang0 <hoang03759@gmail.com>
2023-06-26 17:23:48 +0700

    package gym

--- src/app/main/index/index.component.ts
+++ src/app/main/index/index.component.ts
@@ -29,26 +29,46 @@ export class IndexComponent {
     // Thêm các item khác tương tự
   ];
 
-  imgs: any[] = [
-    { image: '../../../assets/img/img_anime1.jpg', },
-    { image: '../../../assets/img/img_anime2.jpg', },
-    { image: '../../../assets/img/img_anime3.jpg', },
-    { image: '../../../assets/img/img_anime2.jpg', }
-    // Thêm các item khác tương tự
+  // imgs: any[] = [
+  //   { image: '../../../assets/img/img_anime1.jpg', },
+  //   { image: '../../../assets/img/img_anime2.jpg', },
+  //   { image: '../../../assets/img/img_anime3.jpg', },
+  //   { image: '../../../assets/img/img_anime2.jpg', }
+  //   // Thêm các item khác tương tự
+  // ];
+
+  // img1: any[] = [
+  //   { image: '../../../assets/jpg/sky.jpg', },
+  //   { image: '../../../assets/jpg/sky2.jpg', },
+  //   { image: '../../../assets/jpg/sky3.jpg', },
+
+  //   // Thêm các item khác tương tự
+  // ];
+
+  // img3: any[] = [
+  //   { image: '../../../assets/svg/ssshape (2).svg', },
+  //   // Thêm các item khác tương tự
+  // ];
+
+
+  packages: any[] = [
+    { name: "Gym Start", price: "13", expiry: '3',promotion: '7', text: "Get started with creating your own workouts.", image: '../../../assets/img/popup1.png', },
+    { name: "Gym Pro", price: "24", expiry: '6',promotion: '15', text: "Expansion and rapid growth.", image: '../../../assets/img/popup2.png', },
+    { name: "Gym Max", price: "42", expiry: '12',promotion: '30', text: "become a professional !", image: '../../../assets/img/popup3.png', },
   ];
 
-  imgs1: any[] = [
-    { image: '../../../assets/img/img_anime1.jpg', },
-    { image: '../../../assets/img/img_anime2.jpg', },
-    { image: '../../../assets/img/img_anime3.jpg', },
-    { image: '../../../assets/img/img_anime2.jpg', }
-    // Thêm các item khác tương tự
-  ];
+  GymStarts: any[] = [
+    { text: "workout design." },
+    { text: "schedule training." },
+    { text: "contact professional PT." },
+    { text: "private closet." },
+    { text: "schedule exercise equipment." },
+    { text: "manage your exercises." },
+    { text: "health management." },
+    { text: "Unlock advanced tools on the web." },
+    { text: "Free health index measurement 1/week." },
+  ]
 
-  img3: any[] = [
-    { image: '../../../assets/svg/ssshape (2).svg', },
-    // Thêm các item khác tương tự
-  ];
-  
+
 }
 
