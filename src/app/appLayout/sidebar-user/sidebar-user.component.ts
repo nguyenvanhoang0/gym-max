00579fe70a78b774dda0755c17825717e02b8284
nguyenvanhoang0 <hoang03759@gmail.com>
2023-11-05 15:03:12 +0700

    update sidebar

--- src/app/appLayout/sidebar-user/sidebar-user.component.ts
+++ src/app/appLayout/sidebar-user/sidebar-user.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 
+interface GroupSidebar {
+  name: string;
+  img: string;
+  item: SidebarItem[];
+}
+
 
 interface SidebarItem {
-  name: string; 
+  name: string;
   img_1: string;
   img_2: string;
-  showChildItem:boolean;
-  url:string;
+  showChildItem: boolean;
+  url: string;
   items: ChildItem[];
 }
 
 interface ChildItem {
   name: string;
   img: string;
-  url:string;
+  url: string;
 }
 
 @Component({
@@ -24,100 +30,121 @@ interface ChildItem {
 export class SidebarUserComponent {
   isSidebarOpen: boolean = true;
 
-   sidebarData: SidebarItem[] = [
+  sidebarData: GroupSidebar[] = [
+    {
+      name: 'home',
+      img: '',
+      item: [
+        {
+          name: 'Dashboard',
+          img_1: '../../../assets/svg/map.svg',
+          img_2: '../../../assets/svg/Dropdown.svg',
+          showChildItem: false,
+          url: '',
+          items: []
+        }
+      ]
+    },
     {
       name: 'Exercises',
-      img_1: '../../../assets/svg/map.svg',
-      img_2: '../../../assets/svg/Dropdown.svg',
-      showChildItem: false,
-      url: '',
-      items: [
-        {
-          name: 'My Workout',
-          img: '../../../assets/svg/bulk.svg',
-          url: '/user/myWorkout'
-        },
-        {
-          name: 'All Exercises',
-          img: '../../../assets/svg/bulk.svg',
-          url: '/user/allExercises'
-        },
-        {
-          name: 'Created Exercises',
-          img: '../../../assets/svg/bulk.svg',
-          url: '/user/createdExercises'
-        },
-        {
-          name: 'Create Exercises',
-          img: '../../../assets/svg/bulk.svg',
-          url: '/user/createExercises'
-        },
-        {
-          name: 'exercise Content',
-          img: '../../../assets/svg/bulk.svg',
-          url: '/user/exerciseContent'
-        }
-      ]
-    },
-    {
-      name: 'Item 2',
-      img_1: '../../../assets/svg/map.svg',
-      img_2: '../../../assets/svg/Dropdown.svg',
-      showChildItem: false,
-      url: '/main/about',
-      items: [
-        {
-          name: 'About Us',
-          img: '../../../assets/svg/bulk.svg',
-          url: '/main/about-us'
-        },
-        {
-          name: 'Our Team',
-          img: '../../../assets/svg/bulk.svg',
-          url: '/main/our-team'
-        }
-      ]
-    },
-    {
-      name: 'Item 3',
-      img_1: '../../../assets/svg/map.svg',
-      img_2: '../../../assets/svg/Dropdown.svg',
-      showChildItem: false,
-      url: '/main/contact',
-      items: [
-        {
-          name: 'Contact',
-          img: '../../../assets/svg/bulk.svg',
-          url: '/main/contact-us'
-        },
-        {
-          name: 'Location',
-          img: '../../../assets/svg/bulk.svg',
-          url: '/main/location'
-        },
-        {
-          name: 'Hours',
-          img: '../../../assets/svg/bulk.svg',
-          url: '/main/hours'
-        }
-      ]
-    },
-    {
-      name: 'Item 4',
-      img_1: '../../../assets/svg/map.svg',
-      img_2: '../../../assets/svg/Dropdown.svg',
-      showChildItem: false,
-      url: '/main/contact',
-      items: []
+      img: '',
+      item: [{
+        name: 'Exercises',
+        img_1: '../../../assets/svg/map.svg',
+        img_2: '../../../assets/svg/Dropdown.svg',
+        showChildItem: false,
+        url: '',
+        items: [
+          {
+            name: 'My Workout',
+            img: '../../../assets/svg/bulk.svg',
+            url: '/user/myWorkout'
+          },
+          {
+            name: 'All Exercises',
+            img: '../../../assets/svg/bulk.svg',
+            url: '/user/allExercises'
+          },
+          {
+            name: 'Created Exercises',
+            img: '../../../assets/svg/bulk.svg',
+            url: '/user/createdExercises'
+          },
+          {
+            name: 'Create Exercises',
+            img: '../../../assets/svg/bulk.svg',
+            url: '/user/createExercises'
+          },
+          {
+            name: 'exercise Content',
+            img: '../../../assets/svg/bulk.svg',
+            url: '/user/exerciseContent'
+          }
+        ]
+      },
+      {
+        name: 'Item 2',
+        img_1: '../../../assets/svg/map.svg',
+        img_2: '../../../assets/svg/Dropdown.svg',
+        showChildItem: false,
+        url: '/main/about',
+        items: [
+          {
+            name: 'About Us',
+            img: '../../../assets/svg/bulk.svg',
+            url: '/main/about-us'
+          },
+          {
+            name: 'Our Team',
+            img: '../../../assets/svg/bulk.svg',
+            url: '/main/our-team'
+          }
+        ]
+      },
+      {
+        name: 'Item 3',
+        img_1: '../../../assets/svg/map.svg',
+        img_2: '../../../assets/svg/Dropdown.svg',
+        showChildItem: false,
+        url: '/main/contact',
+        items: [
+          {
+            name: 'Contact',
+            img: '../../../assets/svg/bulk.svg',
+            url: '/main/contact-us'
+          },
+          {
+            name: 'Location',
+            img: '../../../assets/svg/bulk.svg',
+            url: '/main/location'
+          },
+          {
+            name: 'Hours',
+            img: '../../../assets/svg/bulk.svg',
+            url: '/main/hours'
+          }
+        ]
+      },
+      {
+        name: 'Item 4',
+        img_1: '../../../assets/svg/map.svg',
+        img_2: '../../../assets/svg/Dropdown.svg',
+        showChildItem: false,
+        url: '/main/contact',
+        items: []
   
-    }
+      }
+    ],
+
+    },
+    
   ];
-  
+
 
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
-    console.log(this.isSidebarOpen );
-    
+    console.log(this.isSidebarOpen);
+
   }
 
   toggleContent(item: any) {
