8ee0f9db2bd68f5e2b4019b1bdb241d22912e9ff
nguyenvanhoang0 <hoang03759@gmail.com>
2024-01-05 15:42:32 +0700

    Upgraded logic ExerciseContent

--- src/app/user/exercise-content/exercise-content.component.ts
+++ src/app/user/exercise-content/exercise-content.component.ts
@@ -1,36 +1,34 @@
-import { Component } from '@angular/core';
-import { MyWorkoutService ,} from '../../../service/my_Workout/my-workout.service';
-import { MyBigExerciseInterface , MyWorkoutData , MonthlyData} from '../../../service/my_Workout/my-workout-interface';
-import { User, UserInterface, AddUserInformation } from '../../../service/user/user-interface';
+import { Component, OnInit } from '@angular/core';
+import { MyWorkoutService } from '../../../service/my_Workout/my-workout.service';
+import { User, UserInterface } from '../../../service/user/user-interface';
 import { UserService } from '../../../service/user/user-service.service';
-import { PracticeTime, PracticeTimes } from '../../../service/practice-time/practice-time-interface';
+import { MonthlyData, MyWorkoutData } from '../../../service/my_Workout/my-workout-interface';
+
 @Component({
   selector: 'app-exercise-content',
   templateUrl: './exercise-content.component.html',
   styleUrls: ['./exercise-content.component.css']
 })
-export class ExerciseContentComponent {
+export class ExerciseContentComponent implements OnInit {
   userInfo: User | null = null;
-  myBigExercises : MyWorkoutData[] = [];
+  myBigExercises: MyWorkoutData[] = [];
   bigExercisesMonthlyData: { [key: string]: MonthlyData[] } = {};
+
   constructor(
     private myWorkoutService: MyWorkoutService,
     private userService: UserService,
   ) {}
-  
+
   ngOnInit(): void {
-    this.UserInfo();
-    // this.calculateDaysInMonth();
+    this.getUserInfo();
   }
 
-  UserInfo() {
+  getUserInfo() {
     this.userService.getUserInfo().subscribe(
       (userInfo) => {
         this.userInfo = userInfo;
         if (this.userInfo?.id !== undefined) {
-          // console.log(this.userInfo?.id);
-          this.GetBigExerciseInMyWorkoutsByUserId(this.userInfo.id);
-          // console.log(this.practiceTimes);
+          this.getBigExerciseInMyWorkoutsByUserId(this.userInfo.id);
         }
       },
       (error) => {
@@ -39,48 +37,35 @@ export class ExerciseContentComponent {
     );
   }
 
-  GetBigExerciseInMyWorkoutsByUserId(id: number) {
+  getBigExerciseInMyWorkoutsByUserId(id: number) {
     this.myWorkoutService.GetBigExerciseInMyWorkoutsByUserId(id).subscribe(
       (response) => {
-        // Xử lý dữ liệu sau khi nhận được từ API
         this.myBigExercises = response.$values;
-        console.log("kkkkk"+this.myBigExercises);
-        
+
         this.myBigExercises.forEach((item) => {
           const timeStart = new Date(item.timeStart);
           const year = timeStart.getFullYear();
           const month = timeStart.getMonth() + 1;
           const day = timeStart.getDate();
-          // Lấy tháng từ 0-11, cần cộng thêm 1
-          const originalDate = "2023-12-30";
-          const parts = originalDate.split('-'); // Tách chuỗi theo dấu '-'
           const modifiedDate = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
-          // Tạo key cho tháng (ví dụ: "2023-10")
           const monthKey = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
-        
-          // Kiểm tra xem key đã tồn tại chưa
+
           if (!this.bigExercisesMonthlyData[monthKey]) {
             this.bigExercisesMonthlyData[monthKey] = [];
           }
-        
-          // Tạo đối tượng dữ liệu mới và push nó vào mảng của tháng tương ứng
+
           const newData: MonthlyData = {
             id: item.id,
             timeStart: modifiedDate,
             bigExercise: {
-              id: item.bigExercise.id,
-              target: item.bigExercise.target,
-              point: item.bigExercise.point,
-              author: item.bigExercise.author,
-              private: item.bigExercise.private,
-              status: item.bigExercise.status,
+              ...item.bigExercise,
             },
           };
-        
+
           this.bigExercisesMonthlyData[monthKey].push(newData);
         });
+
         console.log(this.bigExercisesMonthlyData);
-        
       },
       (error) => {
         console.error(error);
