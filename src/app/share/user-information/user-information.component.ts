67b78de76d07322bca6f5dc03db6a40c944d45c3
nguyenvanhoang0 <hoang03759@gmail.com>
2024-01-17 22:15:52 +0700

    update UserInformation page

--- src/app/share/user-information/user-information.component.ts
+++ src/app/share/user-information/user-information.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component ,Input } from '@angular/core';
 
+import { IUser ,  IAuthor} from '../../../service/user/user-interface';
 @Component({
   selector: 'app-user-information',
   templateUrl: './user-information.component.html',
   styleUrls: ['./user-information.component.css']
 })
 export class UserInformationComponent {
+
+  @Input() user!: IAuthor ;
 
 }
